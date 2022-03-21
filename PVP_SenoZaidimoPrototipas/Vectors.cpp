72b86fbae816077c63fb2eaaacbcb9f8351ad395
GameDream st <gudauskasdovydas@gmail.com>
2022-03-21 18:24:31 +0200

    Vektoriaus randominimas

--- PVP_SenoZaidimoPrototipas/Vectors.cpp
+++ PVP_SenoZaidimoPrototipas/Vectors.cpp
@@ -3,6 +3,31 @@
 
 Vector2 Vector2::up(.0, 1.0), Vector2::down(.0, -1.), Vector2::right(1.0, .0), Vector2::left(-1.0, .0), Vector2::zero(0,0);
 
+Vector2 Vector2::Random()
+{
+	return Vector2::Random(RandomVectorGenerationType::Box);
+}
+
+double RandZeroToOne()
+{
+	return ((double)rand() / (double)RAND_MAX);
+}
+
+double RandOneToOne()
+{
+	return RandZeroToOne() * 2. - 1.;
+}
+
+Vector2 Vector2::Random(RandomVectorGenerationType genType)
+{
+	Vector2 newVec = Vector2(RandOneToOne(), RandOneToOne());
+
+	if (genType == RandomVectorGenerationType::Circle)
+		newVec.Normalize();
+
+	return newVec;
+}
+
 Vector2::Vector2():x(0.), y(0.)
 {
 }
