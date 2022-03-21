bb73939ceea1f943204cad2b4ab35657196773c3
GameDream st <gudauskasdovydas@gmail.com>
2022-03-21 18:24:46 +0200

    Laiko seed'inimas

--- PVP_SenoZaidimoPrototipas/main.cpp
+++ PVP_SenoZaidimoPrototipas/main.cpp
@@ -1,9 +1,11 @@
 
 #include <SDL.h>
 #include <Game.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
+	time(NULL);
 	new Game(); // Pradedam zaidima, visa logika yra Game.h
 
 	return 0;
