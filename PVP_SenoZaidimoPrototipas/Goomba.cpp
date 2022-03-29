e231a7af65b11a5c4378b671589d6efe47f073c9
Ajurkevicius <arvydas.jurkevicius@stud.vilniustech.lt>
2022-03-29 18:36:19 +0300

    Goomba tasku vaizdavimas

--- PVP_SenoZaidimoPrototipas/Goomba.cpp
+++ PVP_SenoZaidimoPrototipas/Goomba.cpp
@@ -1,22 +1,27 @@
 #include <Goomba.h>
-
-Goomba::Goomba() : CollidableSpriteObject(Sprite(Vector2(32, 131), 16, 16))
+#include <Score.h>
+#include <HUD.h>
+Goomba::Goomba() : CollidableSpriteObject(Sprite(Vector2(32, 131), 16, 16)), pointObject(Sprite(), -1)
 {
+	points_height = 50;
+	trigger_cooldown = 0;
+	trigger_delay = 16;
 	speed = 0.5f;
 	goingRight = false;
+ 	pointObject.SetSprite(Sprite(Vector2(0, 280), 21, 7));
 	deathTexture.SetSprite(Sprite(Vector2(32, 131), 16, 16));
 	walkingAnimation.LoadAnimFrames(Sprite(Vector2(0, 131), 16, 16));
 	walkingAnimation.LoadAnimFrames(Sprite(Vector2(16, 131), 16, 16));
 	deathTexture.Enable(false);
+	pointObject.Enable(false);
 	isRemoved = false;
 	ticksAfterRemove = 0;
-
+	//offset.y = 20;
 	Enable(false);
 }
 
 Goomba::~Goomba()
 {
-
 }
 
 void Goomba::OnCollision(CollidableSpriteObject* collision) // override CollidableSpriteObject
@@ -61,7 +66,17 @@ void Goomba::Tick() // override CollidableSpriteObject
 	if (isRemoved) 
 	{
 		SetVelocity(Vector2::zero);
+		
+		if (trigger_cooldown >= 8) {
+			float ratio = trigger_cooldown / (float)trigger_delay;
+			ratio = 1 - ratio;
 
+			positionoffset.y = -sin(ratio * 3.14) * points_height;
+
+			
+			trigger_cooldown--;
+		}
+		pointObject.SetPosition(position + positionoffset);
 		if (ticksAfterRemove < 100)
 			ticksAfterRemove++;
 		else 
@@ -88,16 +103,23 @@ void Goomba::OnPositionChange() // override CollidableSpriteObject
 {
 	CollidableSpriteObject::OnPositionChange();
 
+	
 	deathTexture.SetPosition(position);
 	walkingAnimation.SetPosition(position);
+	pointObject.SetPosition(position + positionoffset);
 }
 
 void Goomba::Remove()
 {
 	if (!isRemoved) 
 	{
+		
+
+		trigger_cooldown = trigger_delay;
 		isRemoved = true;
 		walkingAnimation.EnableRendering(false);
+		pointObject.Enable(true);
+		pointObject.SetPosition(position + positionoffset);
 		deathTexture.Enable(true);
 		EnableCollision(false);
 	}
